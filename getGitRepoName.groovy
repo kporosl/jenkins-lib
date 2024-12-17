e0fdf369075bf6ce4381331c99f223d8177635ee
kappa <52362693+kporosl@users.noreply.github.com>
2024-12-17 11:30:04 +0900

    Delete getGitRepoName.groovy

--- getGitRepoName.groovy
+++
@@ -1,6 +0,0 @@
-def call() {
-    def gitRepoUrl = scm.getUserRemoteConfigs()[0].getUrl()
-    def gitRepoName = gitRepoUrl.tokenize('/').last().replaceAll(/\.git$/, '')
-	
-    return gitRepoName
-}
